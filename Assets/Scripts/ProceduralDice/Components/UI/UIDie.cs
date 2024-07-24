f291efb2983a1c6419f1c5ce1b52723eb239cff3
Alexiush <streltsov2609@gmail.com>
2024-07-24 18:14:12 +0300

    Moved UI to separate class Added special editor window for die prerendering instead of using UIDie for this

---
+++ Assets/Scripts/ProceduralDice/Components/UI/UIDie.cs
@@ -0,0 +1,122 @@
+using Nothke.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Unity.Jobs;
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.U2D.Sprites;
+using UnityEngine.Rendering;
+using TMPro;
+
+[RequireComponent(typeof(Image))]
+public class UIDie : DiceRotatorBase
+{
+    public UIDieData dieData;
+    private Image _die;
+
+    public override int SidesCount => dieData.SidesTextures.Count;
+
+    [SerializeField]
+    private int _seed;
+    [SerializeField]
+    private int _defaultSide;
+
+    [SerializeField]
+    private bool _explicitNumbers;
+    public bool ExplicitNumbers
+    {
+        get
+        {
+            return _explicitNumbers;
+        }
+        set
+        {
+            _explicitNumbers = value;
+            _numbersLabel.enabled = _explicitNumbers;
+        }
+    }
+
+    [SerializeField]
+    private TextMeshProUGUI _numbersLabel;
+
+    private void Awake()
+    {
+        UnityEngine.Random.InitState(_seed);
+        _die = GetComponent<Image>();
+
+        _numbersLabel.text = string.Empty;
+    }
+
+    private void Start()
+    {
+        RotateDie(_defaultSide);
+    }
+
+    public override void RotateDie(int side)
+    {
+        if (_rotating)
+        {
+            Debug.Log("Already rotating");
+            return;
+        }
+
+        _die.sprite = dieData.SidesTextures[side - 1];
+
+        if (_explicitNumbers)
+        {
+            _numbersLabel.text = side.ToString();
+        }
+    }
+
+    private bool _rotating;
+    private float _timeLeft;
+    private int _frame;
+    private int _processedTargetSide;
+
+    public override void RotateDieAnimated(int side, float animationDuration)
+    {
+        if (_rotating)
+        {
+            Debug.Log("Already rotating");
+            return;
+        }
+
+        _frame = 0;
+        _timeLeft = animationDuration;
+        _rotating = true;
+        _processedTargetSide = side;
+    }
+
+    private void Update()
+    {
+        if (!_rotating)
+        {
+            return;
+        }
+
+        _timeLeft -= Time.deltaTime;
+
+        if (_timeLeft > 0)
+        {
+            _die.sprite = dieData.AnimationFrames[_frame % dieData.AnimationFrames.Count];
+
+            if (_explicitNumbers)
+            {
+                _numbersLabel.text = UnityEngine.Random.Range(1, SidesCount + 1).ToString();
+            }
+            
+            _frame++;
+        }
+        else
+        {
+            _rotating = false;
+            RotateDie(_processedTargetSide);
+        }
+    }
+}
---
+++ Assets/Scripts/ProceduralDice/Components/UI/UIToolkitDie.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIToolkitDie : MonoBehaviour
+{
+    [SerializeField]
+    private UIDocument _document;
+    
+    [SerializeField]
+    private UIElementDie _die;
+    [SerializeField]
+    private UIDieData _dieData;
+    
+    [SerializeField]
+    private Button _rotateButton;
+    [SerializeField]
+    private bool _explicitNumbers;
+
+    private void Start()
+    {
+        _die = _document.rootVisualElement.Q("UIDie") as UIElementDie;
+        _die.ExplicitNumbers = _explicitNumbers;
+        _die.Initialize(_dieData);
+
+        _rotateButton = _document.rootVisualElement.Q("Rotate") as Button;
+        _rotateButton.clicked += () => _die.RotateDieAnimated(UnityEngine.Random.Range(1, _die.SidesCount + 1), 1f);
+    }
+}
